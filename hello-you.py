582a494690c1a10f7fe4e88d0501391be9a68891
ben-butera <ben.butera@gmail.com>
2024-12-11 15:49:31 -0500

    Update hello-you.py

--- hello-you.py
+++ hello-you.py
@@ -2,7 +2,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("your_name")
+    parser.add_argument('--your_name')
     args = parser.parse_args()
 
     runner_name = args.your_name
