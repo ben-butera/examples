9d52a8956c025beedd5db63d2517497446d04e22
ben-butera <ben.butera@gmail.com>
2024-12-11 15:20:38 -0500

    Update hello-you.py

--- hello-you.py
+++ hello-you.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-def main():
+
+
+def main(your_name):
     print("Hello ", your_name, "!")
     print("For more information on Itential Automation Service, check out:")
     print("https://docs.itential.com/itential-cloud/docs/announcements")
 
 if __name__ == "__main__":
-    main()
+    main(your_name)
